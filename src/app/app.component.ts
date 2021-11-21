c46f288095fa6d01cde27935bcb25cb48b945efe
haerulmutakin <haerulm.takin@gmail.com>
2021-11-21 20:31:01 +0700

    call child component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'student-management';
+export class AppComponent implements OnInit {
+  constructor( ) {}
+
+  ngOnInit() {}
+
 }
