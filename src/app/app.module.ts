9d0b29d0bf8ff5e0edd90ba3330c7aa91833d051
haerulmutakin <haerulm.takin@gmail.com>
2021-11-21 20:29:02 +0700

    add angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { NgSelectModule } from '@ng-select/ng-select';
 
-import { AppRoutingModule } from './app-routing.module';
+import { CorrectingOptPipe } from './pipes/correcting-opt.pipe';
+import { UnassignedTotalPipe } from './pipes/unassigned-total.pipe';
 import { AppComponent } from './app.component';
+import { StudentTableComponent } from './student-table/student-table.component';
+import { SchoolTableComponent } from './school-table/school-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CorrectingOptPipe,
+    UnassignedTotalPipe,
+    StudentTableComponent,
+    SchoolTableComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatTableModule,
+    MatPaginatorModule,
+    NgSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
